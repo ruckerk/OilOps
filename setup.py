af54126b21e922fe3999c6e1f2e673757f8732b6
ruckerk <67653134+ruckerk@users.noreply.github.com>
2022-03-24 14:39:36 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='UWI',
     url='https://github.com/ruckerk/PetroOps'
-    author='W Kurt Rucker',
+    author='W_Kurt_Rucker',
     author_email='william.rucker@gmail.com',
     packages=['UWI'],
     install_requires=['numpy', 'math'],
