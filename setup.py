2b1464a16fd6f99838a55718fdd3088c83b78e0b
ruckerk <67653134+ruckerk@users.noreply.github.com>
2022-03-24 15:42:12 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author='W Kurt Rucker',
     author_email='william.rucker@gmail.com',
     packages=['PetroOps'],
-    install_requires=['numpy', 're'],
+    install_requires=['numpy'],
     python_requires='>3.6.2',
     license='MIT',
     description='Oilfield Operations Tools',
