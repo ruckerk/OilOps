43f03b7c7635c32d69f41030a088c1d58ae9b68d
ruckerk <67653134+ruckerk@users.noreply.github.com>
2022-03-24 14:26:21 -0600

    Create setup.py
    create file

---
+++ UWI/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='UWI',
+    url='https://github.com/ruckerk/PetroOps'
+    author='W Kurt Rucker',
+    author_email='william.rucker@gmail.com',
+    packages=['UWI'],
+    install_requires=['numpy', 'math'],
+    license='MIT',
+    description='Oilfield API string manager',
+    long_description=open('README.txt').read()
+)
