e7967667c587a9f7b9ff2b507c1ab9ec0372e8e3
ruckerk <67653134+ruckerk@users.noreply.github.com>
2022-03-24 14:40:04 -0600

    Update setup.py
    add missing comma

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup
 
 setup(
     name='UWI',
-    url='https://github.com/ruckerk/PetroOps'
-    author='W_Kurt_Rucker',
+    url='https://github.com/ruckerk/PetroOps',
+    author='W Kurt Rucker',
     author_email='william.rucker@gmail.com',
     packages=['UWI'],
     install_requires=['numpy', 'math'],
