186f7fedea13c6f3319f5bcd1bf780640de94164
ruckerk <67653134+ruckerk@users.noreply.github.com>
2022-03-24 22:51:30 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='PetroOps',
-    version = '0.0.1',
+    version = '0.0.2',
     url='https://github.com/ruckerk/PetroOps',
     author='W Kurt Rucker',
     author_email='william.rucker@gmail.com',
