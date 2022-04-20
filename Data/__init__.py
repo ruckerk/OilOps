779287b6db7d699b4981e2523e6a01478cf8affe
ruckerk <67653134+ruckerk@users.noreply.github.com>
2022-04-20 16:17:17 -0600

    Create __init__.py

---
+++ Data/__init__.py
@@ -0,0 +1,6 @@
+from .BaseData import BaseData
+from .CoLAS import CoLAS
+from .CoProduction import CoProduction
+from .CoScout import CoScout
+from .CoSurveys import CoSurveys
+from .FracFocus  import FracFocus
