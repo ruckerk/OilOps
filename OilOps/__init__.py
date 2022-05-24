eacbaf9fe4cb5574d18a5a02265d944613dcb0a0
ruckerk <67653134+ruckerk@users.noreply.github.com>
2022-05-24 16:18:36 -0600

    Update __init__.py

--- OilOps/__init__.py
+++ OilOps/__init__.py
@@ -1,6 +1,1 @@
-import .OilOps.DATA
-import .OilOps.Gunbarrel
-import .OilOps.OneLine
-import .OilOps.SUMMARIZE_COGCC_SQL
-import .OilOps.SurveysFromPDF
-import .OilOps.WELLAPI
+from .OilOps import *
