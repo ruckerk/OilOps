3b4e56f01295aaa03b5361e8a29e190dfd8075de
ruckerk <67653134+ruckerk@users.noreply.github.com>
2022-05-25 08:50:57 -0600

    Delete DATA.py

--- OilOps/DATA.py
+++
@@ -1,7 +0,0 @@
-from .DATA import CO_BASEDATA
-from .DATA import Get_LAS
-from .DATA import Get_ProdData
-from .DATA import Get_Scouts
-from .DATA import Merge_Frac_Focus
-from ._FUNCS_ import DF_UNSTRING
-from .OneLine import ONELINE_COMPILE
