da63aadc4962c5d90bf2a948073f752f54d53e0c
ruckerk <67653134+ruckerk@users.noreply.github.com>
2022-05-25 08:33:29 -0600

    Update __init__.py

--- OilOps/__init__.py
+++ OilOps/__init__.py
@@ -1,7 +1,3 @@
 #from .OilOps import *
-from .DATA import CO_BASEDATA
-from .DATA import Get_LAS
-from .DATA import Get_ProdData
-from .DATA import Get_Scouts
-from .DATA import Merge_Frac_Focus
+import .DATA
 from .Gunbarrel import STAIR_PLOT
