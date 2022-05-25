7078a43d1e06c23c721e5d6327b7b01fb02cbf0c
ruckerk <67653134+ruckerk@users.noreply.github.com>
2022-05-25 09:03:24 -0600

    Delete __init__.py

--- OilOps/Data/__init__.py
+++
@@ -1,6 +0,0 @@
-from .BaseData import BaseData
-from .CoLAS import CoLAS
-from .CoProduction import CoProduction
-from .CoScout import CoScout
-from .CoSurveys import CoSurveys
-from .FracFocus  import FracFocus
