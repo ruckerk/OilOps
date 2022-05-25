4c1ceb46a83ccb47f46dbb098ab5ac8950c3281b
ruckerk <67653134+ruckerk@users.noreply.github.com>
2022-05-25 08:36:10 -0600

    Create DATA.py

---
+++ OilOps/DATA.py
@@ -0,0 +1,7 @@
+from .DATA import CO_BASEDATA
+from .DATA import Get_LAS
+from .DATA import Get_ProdData
+from .DATA import Get_Scouts
+from .DATA import Merge_Frac_Focus
+from ._FUNCS_ import DF_UNSTRING
+
