4d0b81c56b41951554bf0653c8dd5884269f9f3e
ruckerk <67653134+ruckerk@users.noreply.github.com>
2022-05-26 16:08:31 -0600

    Create _SURVEYFUNCS_.py

---
+++ OilOps/_SURVEYFUNCS_.py
@@ -0,0 +1,15 @@
+from .WELLAPI import WELLAPI as WELLAPI
+import numpy as np
+import pandas as pd
+import sys
+import re
+import math
+import shutil
+from os import path, listdir, remove, makedirs, rename
+from math import ceil,isnan,floor
+import multiprocessing
+import concurrent.futures
+from functools import partial
+from datetime import datetime
+
+
