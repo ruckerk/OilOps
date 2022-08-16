08c671055a33391ccb61400e2a2e9bf4cf4148a4
ruckerk <67653134+ruckerk@users.noreply.github.com>
2022-08-16 12:02:30 -0600

    Update DATA.py
    glob.glob back to glob
    Add conditional flag for CO_Data pull

--- OilOps/DATA.py
+++ OilOps/DATA.py
@@ -3,58 +3,60 @@ from ._FUNCS_ import *
 
 #Define Functions for multiprocessing iteration
 
-def CO_BASEDATA():
+def CO_BASEDATA(FRACFOCUS = True, COGCC_SQL = True, COGCC_SHP = True):
     pathname = path.dirname(argv[0])
     adir = path.abspath(pathname)
 
     #Frac Focus
-    #https://www.fracfocus.org/index.php?p=data-download
-    url = 'https://fracfocusdata.org/digitaldownload/FracFocusCSV.zip'
-    filename = wget.download(url)
-    with ZipFile(filename, 'r') as zipObj:
-       # Extract all the contents of zip file in current directory
-       zipObj.extractall('FRAC_FOCUS')
+    if FRACFOCUS:
+        #https://www.fracfocus.org/index.php?p=data-download
+        url = 'https://fracfocusdata.org/digitaldownload/FracFocusCSV.zip'
+        filename = wget.download(url)
+        with ZipFile(filename, 'r') as zipObj:
+           # Extract all the contents of zip file in current directory
+           zipObj.extractall('FRAC_FOCUS')
 
 
     # COGCC SQLITE
     # https://dnrftp.state.co.us/
-    url = 'https://dnrftp.state.co.us/COGCC/Temp/Gateway/CO_3_2.1.zip'
-    filename = wget.download(url)
-    with ZipFile(filename, 'r') as zipObj:
-       # Extract all the contents of zip file in current directory
-       zipObj.extractall('COOGC_SQL')
+    if COGCC_SQL:
+        url = 'https://dnrftp.state.co.us/COGCC/Temp/Gateway/CO_3_2.1.zip'
+        filename = wget.download(url)
+        with ZipFile(filename, 'r') as zipObj:
+           # Extract all the contents of zip file in current directory
+           zipObj.extractall('COOGC_SQL')
 
-    files = []
-    start_dir = path.join(adir,'COOGC_SQL')
-    pattern   = r'CO_3_2.*'
+        files = []
+        start_dir = path.join(adir,'COOGC_SQL')
+        pattern   = r'CO_3_2.*'
+        for dir,_,_ in walk(start_dir):
+            files.extend(glob(path.join(dir,pattern)))
 
-    for dir,_,_ in walk(start_dir):
-        files.extend(glob.glob(path.join(dir,pattern)))
-
-    shutil.move(files[0], path.join(adir, path.basename(files[0])))
-    shutil.rmtree(path.join(adir,'COOGC_SQL'))
+        shutil.move(files[0], path.join(adir, path.basename(files[0])))
+        shutil.rmtree(path.join(adir,'COOGC_SQL'))
 
     # COGCC shapefiles
-    url = 'https://cogcc.state.co.us/documents/data/downloads/gis/DIRECTIONAL_LINES_SHP.ZIP'
-    filename = wget.download(url)
-    with ZipFile(filename, 'r') as zipObj:
-       # Extract all the contents of zip file in current directory
-       zipObj.extractall()
-    remove(filename)
+    if COGCC_SHP:
+        url = 'https://cogcc.state.co.us/documents/data/downloads/gis/DIRECTIONAL_LINES_SHP.ZIP'
+        filename = wget.download(url)
+        with ZipFile(filename, 'r') as zipObj:
+           # Extract all the contents of zip file in current directory
+           zipObj.extractall()
+        remove(filename)
 
-    url = 'https://cogcc.state.co.us/documents/data/downloads/gis/DIRECTIONAL_LINES_PENDING_SHP.ZIP'
-    filename = wget.download(url)
-    with ZipFile(filename, 'r') as zipObj:
-       # Extract all the contents of zip file in current directory
-       zipObj.extractall()
-    remove(filename)
+        url = 'https://cogcc.state.co.us/documents/data/downloads/gis/DIRECTIONAL_LINES_PENDING_SHP.ZIP'
+        filename = wget.download(url)
+        with ZipFile(filename, 'r') as zipObj:
+           # Extract all the contents of zip file in current directory
+           zipObj.extractall()
+        remove(filename)
 
-    url = 'https://cogcc.state.co.us/documents/data/downloads/gis/WELLS_SHP.ZIP'
-    filename = wget.download(url)
-    with ZipFile(filename, 'r') as zipObj:
-       # Extract all the contents of zip file in current directory
-       zipObj.extractall()
-    remove(filename)
+        url = 'https://cogcc.state.co.us/documents/data/downloads/gis/WELLS_SHP.ZIP'
+        filename = wget.download(url)
+        with ZipFile(filename, 'r') as zipObj:
+           # Extract all the contents of zip file in current directory
+           zipObj.extractall()
+        remove(filename)
 
 def Get_LAS(UWIS):
     #if 1==1:
