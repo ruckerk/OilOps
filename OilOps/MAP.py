3413e296332f29678909dee30fbdf1991d7c00b7
ruckerk <67653134+ruckerk@users.noreply.github.com>
2022-09-07 17:02:00 -0600

    Remedy common shapely geometries failing

--- OilOps/MAP.py
+++ OilOps/MAP.py
@@ -176,9 +176,11 @@ def SHP_DISTANCES(SHP1,SHP2,MAXDIST=10000,CALCEPSG = 26753):
 
     _GS1_RPRJ = shapely.ops.transform(project1, _GS1)
     _GS2_RPRJ = shapely.ops.transform(project2, _GS2)
+	
+    WORKLIST = [geom if geom.is_valid else geom.buffer(0) for geom in _GS2_RPRJ]
 
-    boundary = shapely.ops.unary_union([x.buffer(MAXDIST) for x in _GS2_RPRJ.geoms])
-    boundary0 = shapely.ops.unary_union(_GS2_RPRJ.geoms)
+    boundary = shapely.ops.unary_union([x.buffer(MAXDIST) for x in WORKLIST])
+    boundary0 = shapely.ops.unary_union(WORKLIST)
     
     # MAINSHAPE  DLINES = shp.Reader('Directional_Lines.prj')
     # MAIN_DF   dfs = read_shapefile(DLINES)
