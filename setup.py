497f7bdb338c3a354e99d877f9542625527c25f6
ruckerk <67653134+ruckerk@users.noreply.github.com>
2022-09-11 23:44:17 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,6 @@ setup(
     packages=['OilOps'],
     install_requires=['adjustText', 
                       'bs4', 
-                      'futures',
                       'easygui', 
                       'futures3',
                       'matplotlib', 
