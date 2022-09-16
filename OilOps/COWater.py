f6fa20ad60e20692c10463a535e57beb6f0719a6
ruckerk <67653134+ruckerk@users.noreply.github.com>
2022-09-15 22:03:56 -0600

    Update COWater.py

--- OilOps/COWater.py
+++ OilOps/COWater.py
@@ -119,3 +119,75 @@ def DWR_WATERWELLLEVELS(LAT,LON, RADIUS = 1, RADIUS_UNIT = 'miles'):
         for k in QTERMS:
             df.loc[i,k] = r[k]
     return(df)
+
+
+def WaterDataPull(LAT=40.5832238,LON=-104.0990673,RADIUS=10):
+    # Lat/Lon as WGS84
+    # RADIUS in miles
+    
+    headers = {
+        'Accept': 'application/zip',
+    }
+
+    params = (
+        ('mimeType', 'xlsx'),
+        ('zip', 'yes'),
+    )
+
+    json_data = {
+        'within': 30,
+        'lat': str(LAT),
+        'long': str(LON),
+        'siteType': [
+            'Well',
+            'Subsurface',
+            'Facility',
+            'Aggregate groundwater use',
+            'Not Assigned',
+        ],
+        'sampleMedia': [
+            'Water',
+            'water',
+            'Other',
+            'No media',
+        ],
+        'characteristicName': [
+            'Fixed dissolved solids',
+            'Total dissolved solids',
+            'Total solids',
+            'Solids',
+            'Dissolved solids',
+            'Total fixed solids',
+            'Fixed suspended solids',
+            'Percent Solids',
+            'Total suspended solids',
+            'Salinity',
+        ],
+        'startDateLo': '01-01-1900',
+        'startDateHi': datetime.datetime.now().strftime("%m-%d-%Y"),
+        'dataProfile': 'resultPhysChem',
+        'providers': [
+            'NWIS',
+            'STEWARDS',
+            'STORET',
+        ],
+    }
+
+    #response = requests.get('https://www.waterqualitydata.us/data/Result/search', headers=headers, params=params, stream = True)
+    r_data = requests.post('https://www.waterqualitydata.us/data/Result/search', headers=headers, params=params, json=json_data)
+    #r_station = requests.post('https://www.waterqualitydata.us/data/Station/search', headers=headers, params=params, json=json_data)
+
+    r_station = requests.get("https://www.waterqualitydata.us/data/Station/search?within=" + str(RADIUS) + "&lat=" +  str(LAT)+'&long=' + str(LON) + "&siteType=Well&siteType=Subsurface&siteType=Facility&siteType=Aggregate%20groundwater%20use&siteType=Not%20Assigned&sampleMedia=Water&sampleMedia=water&sampleMedia=Other&sampleMedia=No%20media&characteristicName=Total%20dissolved%20solids&characteristicName=Dissolved%20solids&characteristicName=Total%20solids&characteristicName=Total%20suspended%20solids&characteristicName=Fixed%20dissolved%20solids&characteristicName=Fixed%20suspended%20solids&characteristicName=Solids&characteristicName=Percent%20Solids&characteristicName=Total%20fixed%20solids&characteristicName=Salinity&startDateLo=01-01-1900&startDateHi=01-01-2030&mimeType=xlsx&zip=yes&providers=NWIS&providers=STEWARDS&providers=STORET")
+    
+    #zipfile = ZipFile(BytesIO(r.content))
+    #f = zipfile.namelist()[0]
+    #pd.read_excel(zipfile.open(f,mode = 'r')).keys()
+    
+    
+    # Note: original query string below. It seems impossible to parse and
+    # reproduce query strings 100% accurately so the one below is given
+    # in case the reproduced version is not "correct".
+    #response = requests.post('https://www.waterqualitydata.us/data/Result/search?mimeType=csv&zip=yes', headers=headers, json=json_data)
+    return r_data,r_station
+
+
