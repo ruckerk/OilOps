827b976c6abaa4f058a6926481a8079993a5d05d
ruckerk <67653134+ruckerk@users.noreply.github.com>
2022-09-15 22:31:14 -0600

    Update COWater.py

--- OilOps/COWater.py
+++ OilOps/COWater.py
@@ -1,5 +1,5 @@
 from ._FUNCS_ import *
-from .MAP import Pt_Distance, Pt_Bearing
+from .MAP import Pt_Distance, Pt_Bearing, county_from_LatLon
 
 def DWR_GEOPHYSWELLSUMMARY(LAT,LON, RADIUS = 1, RADIUS_UNIT = 'miles'):
     # EXPECTING WGS84 COORDINATES
