6e88c677837c5a803fd99272935582d583b99514
ruckerk <67653134+ruckerk@users.noreply.github.com>
2022-09-15 22:52:33 -0600

    Update MAP.py

--- OilOps/MAP.py
+++ OilOps/MAP.py
@@ -252,7 +252,7 @@ def Pt_Distance(pt1,pt2):
     dlon = lon2-lon1
     dlat = lat2-lat1
     a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
-    c = 2 * atan2(math.sqrt(a), sqrt(1 - a))
+    c = 2 * atan2(sqrt(a), sqrt(1 - a))
     distance = R * c
     return(distance)
 
