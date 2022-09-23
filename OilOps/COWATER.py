7b98cb1815a0ed8f539cce4fd655adab842059a8
ruckerk <67653134+ruckerk@users.noreply.github.com>
2022-09-23 10:28:00 -0600

    Update COWATER.py
    index repair
    m = df_permits.loc[m1.join(m2),'depthTotal'].dropna().index

--- OilOps/COWATER.py
+++ OilOps/COWATER.py
@@ -317,7 +317,9 @@ def CO_WATERWELL_SUMMARY(LAT,LON,RADIUS = 1,UNITS = 'miles', EPSG_IN = 4269, DAT
     m2 = df_permits['depthTotal'].dropna().index
     df_permits['MAX_DEPTH'] = df_permits.loc[m1,'loc'].map(df_permits.loc[m1,:].groupby(by='loc')['bottomPerforatedCasing'].max().dropna())
     m3 = df_permits['MAX_DEPTH'].dropna().index
-    m = m2.drop(m1.join(m2))
+    
+    m = df_permits.loc[m1.join(m2),'depthTotal'].dropna().index
+    
     df_permits.loc[df_permits.index.drop(m3),'MAX_DEPTH'] = df_permits.loc[m,'loc'].map(df_permits.loc[m,:].groupby(by='loc')['bottomPerforatedCasing'].max().dropna())
     
     m = df_permits['MAX_DEPTH'].dropna().index
