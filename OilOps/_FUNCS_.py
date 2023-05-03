abafec3d553935ba877f5ff0793d639a96e98128
ruckerk <67653134+ruckerk@users.noreply.github.com>
2023-05-03 16:24:58 -0600

    Update _FUNCS_.py

--- OilOps/_FUNCS_.py
+++ OilOps/_FUNCS_.py
@@ -1,5 +1,5 @@
 # requires xlrd, openpyxl
-from adjusttext import adjust_text
+from adjustText import adjust_text
 from bs4 import BeautifulSoup as BS
 from functools import partial
 from io import StringIO, BytesIO
