856a169d8cbc53d8ce16de29b247b213419711ed
ruckerk <67653134+ruckerk@users.noreply.github.com>
2023-07-25 02:51:40 -0500

    Update GUNBARREL.py

--- OilOps/GUNBARREL.py
+++ OilOps/GUNBARREL.py
@@ -8,20 +8,26 @@ __all__ = ['STAIR_PLOT']
 def STAIR_PLOT(ULIST,df, ProdKey= None):
 
     # subset or wells in plotting order
-    m = df.loc[df.UWI10.isin(ULIST)].MeanX.dropna().sort_values(ascending=True).index
-    #m = df.loc[m,'StateProducingUnitKey'].dropna().index
+    #XY_KEY = GetKey(df,'Mean.*TVD')[0]
+    #m = df.loc[df.UWI10.isin(ULIST)][XY_KEY].dropna().sort_values(ascending=True).index
 
+    #Select hz dimension
+    XY_KEY = df.loc[df.UWI10.isin(ULIST),GetKey(df,'Mean.*(X|Y)')].diff().abs().mean(axis=0).sort_values(ascending=False).keys()[0]
+
+    # subset or wells in plotting order
+    m = df.loc[df.UWI10.isin(ULIST),XY_KEY].dropna().sort_values(ascending=True).index
+    
     # normalize X axis
-    minX = df.loc[m,'MeanX'].min()
+    minX = df.loc[m,XY_KEY].min()
 
     # normalize color scale
     if ProdKey:
-        CVALS = ((df.loc[m,ProdKey] - df.loc[m,ProdKey].min()) / (df.loc[m,ProdKey].max()-df.loc[m,ProdKey].min())).values
+        #CVALS = ((df.loc[m,ProdKey] - df.loc[m,ProdKey].min()) / (df.loc[m,ProdKey].max()-df.loc[m,ProdKey].min())).values
         CVALS = df.loc[m,ProdKey].values
         CVAL_STEP = 10**np.floor(np.log10(df.loc[m,ProdKey].max() - df.loc[m,ProdKey].min()))/2
-        CVAL_RANGE = range(int(np.ceil(CVALS.min()/CVAL_STEP)*CVAL_STEP),
-                       int(np.ceil(CVALS.max()/CVAL_STEP)*CVAL_STEP),
-                       int(CVAL_STEP)) 
+        CVAL_RANGE = range(int(np.ceil(np.nanmin(CVALS)/CVAL_STEP)*CVAL_STEP),
+                       int(np.ceil(np.nanmax(CVALS)/CVAL_STEP)*CVAL_STEP),
+                       int(CVAL_STEP))
     else:
         CVALS = 0
     
@@ -29,13 +35,13 @@ def STAIR_PLOT(ULIST,df, ProdKey= None):
     #plot gunbarrel and stairs
     fig, ax = plt.subplots(1, sharex = True, squeeze = True)
 
-    fig.set_figheight(5)
-    fig.set_figwidth(12)
+    fig.set_figheight(7)
+    fig.set_figwidth(18)
 
     #adjust Y axis for label room
     plt.ylim([df.loc[m,'MeanTVD'].min()-100, df.loc[m,'MeanTVD'].max()+100])
-
-    ax.step(df.loc[m,'MeanX'] - minX,
+    
+    ax.step(df.loc[m,XY_KEY] - minX,
             df.loc[m,'MeanTVD'],
             where = 'post',
             linewidth=1,
@@ -43,7 +49,7 @@ def STAIR_PLOT(ULIST,df, ProdKey= None):
             alpha = 0.8)
 
     # add color by EUR
-    sc = ax.scatter(df.loc[m,'MeanX'] - minX,
+    sc = ax.scatter(df.loc[m,XY_KEY] - minX,
             df.loc[m,'MeanTVD'],
             c = CVALS,
             marker = 'o',
@@ -63,9 +69,9 @@ def STAIR_PLOT(ULIST,df, ProdKey= None):
     # fill grid cell and label scale
 
     # plot horizontal distances
-    X = ((df.loc[m,'MeanX'] - minX).iloc[:-1] + df.loc[m,'MeanX'].diff().dropna().values/2).values
+    X = ((df.loc[m,XY_KEY] - minX).iloc[:-1] + df.loc[m,XY_KEY].diff().dropna().values/2).values
     Y = (df.loc[m,'MeanTVD'].iloc[:-1]).values
-    LAB = df.loc[m,'MeanX'].diff().dropna().abs().apply(np.floor).values.astype(int)
+    LAB = df.loc[m,XY_KEY].diff().dropna().abs().apply(np.floor).values.astype(int)
 
     for i in range(0,len(X)):
         plt.text(X[i],Y[i],LAB[i],
@@ -76,7 +82,7 @@ def STAIR_PLOT(ULIST,df, ProdKey= None):
                  verticalalignment = 'center');
 
     # plot vertical distances
-    X = (df.loc[m,'MeanX'] - minX).iloc[1:].values
+    X = (df.loc[m,XY_KEY] - minX).iloc[1:].values
     Y = (df.loc[m,'MeanTVD'].iloc[1:]-df.loc[m,'MeanTVD'].diff().dropna()/2).values
     LAB = df.loc[m,'MeanTVD'].diff().dropna().abs().apply(np.floor).values.astype(int)
 
@@ -93,7 +99,7 @@ def STAIR_PLOT(ULIST,df, ProdKey= None):
     LABELFORM = "API:_API_ DATE:_DATE_\nNAME:_NAME_\nOPER:_OPER_\nFI:_FI_   PI:_PI_\nLL:_LATLEN_   PROD:_PROD_"
 
     WELLDATE = GetKey(df,r'FIRST.*PROD.*DATE|JOB.*DATE|SPUD.*DATE')
-    WELLDATE = df.loc[m,WELLDATE].keys()[(df.loc[m,WELLDATE].isna().count(axis=0) == df.loc[m,WELLDATE].isna().count(axis=0).min())].tolist()
+    WELLDATE = df.loc[m,WELLDATE].keys()[(df.loc[m,WELLDATE].isna().sum(axis=0) == df.loc[m,WELLDATE].isna().sum(axis=0).min())].tolist()
     WELLDATE = df.loc[m,WELLDATE].dropna().apply(lambda x:x[WELLDATE] == max(x[WELLDATE]), axis =1).max(axis=0).sort_values(ascending = False).keys()[0]
     WELL_LABEL = GetKey(df,r'WELL.*(NAME|LABEL|NO)')
     test = df.loc[m,WELL_LABEL].applymap(lambda x: len(str(x)))
@@ -109,10 +115,13 @@ def STAIR_PLOT(ULIST,df, ProdKey= None):
             LABELS = list()
         
         LABEL_TEXT = LABELFORM.replace('_API_',str(df.loc[i,'UWI10']))
-        LABEL_TEXT = LABEL_TEXT.replace('_DATE_',df.loc[i,WELLDATE].strftime('%Y-%m-%d'))
+        try:
+            LABEL_TEXT = LABEL_TEXT.replace('_DATE_',df.loc[i,WELLDATE].strftime('%Y-%m-%d'))
+        except:
+            LABEL_TEXT = LABEL_TEXT.replace('_DATE_','NA')
         LABEL_TEXT = LABEL_TEXT.replace('_NAME_',df.loc[i,WELL_LABEL].strip())
         LABEL_TEXT = LABEL_TEXT.replace('_OPER_',df.loc[i,OPERATOR].strip())
-        LABEL_TEXT.replace('_FI_',df.loc[i,FLUID_INTENSITY].astype(int).astype(str).strip()+' BBL/FT')
+        LABEL_TEXT = LABEL_TEXT.replace('_FI_',df.loc[i,FLUID_INTENSITY].astype(int).astype(str).strip()+' BBL/FT')
         LABEL_TEXT = LABEL_TEXT.replace('_PI_',df.loc[i,PROP_INTENSITY].astype(int).astype(str).strip()+' #/FT')
         LABEL_TEXT = LABEL_TEXT.replace('_LATLEN_',df.loc[i,LATERAL_LENGTH].astype(int).astype(str).strip()+' FT')
         if ProdKey:
@@ -124,7 +133,7 @@ def STAIR_PLOT(ULIST,df, ProdKey= None):
         LABELS.append(LABEL_TEXT)
         
     # manage overposting
-    texts = [ax.text((df.loc[m[j],'MeanX'] - minX),
+    texts = [ax.text((df.loc[m[j],XY_KEY] - minX),
                      df.loc[m[j],'MeanTVD'],
                      LABELS[j].strip(),
              fontsize = 9,
