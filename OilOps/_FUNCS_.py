00929d1a5a05ab52ff70721f0b7ef150dd981696
ruckerk <67653134+ruckerk@users.noreply.github.com>
2023-10-29 22:46:04 -0600

    Update _FUNCS_.py

--- OilOps/_FUNCS_.py
+++ OilOps/_FUNCS_.py
@@ -49,9 +49,8 @@ import selenium
 import shapefile as shp #pyshp
 import sklearn as sk
 from sklearn.decomposition import PCA
-from sklearn.cluster import HDBSCAN 
-import sqlalchemy
-import sqlite3
+from sklearn.cluster import HDBSCAN
+import sqlalchemyimport sqlite3
 import urllib
 import warnings
 import wget
