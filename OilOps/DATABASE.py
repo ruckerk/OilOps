065326b441a1e1c9b796eaa60682de1a82228803
ruckerk <67653134+ruckerk@users.noreply.github.com>
2023-11-07 09:37:23 -0600

    Update DATABASE.py

--- OilOps/DATABASE.py
+++ OilOps/DATABASE.py
@@ -528,8 +528,7 @@ def UPDATE_SURVEYS(DB = 'FIELD_DATA.db', FULL_UPDATE = False, FOLDER = 'SURVEYFO
     # Initialize constants
     URL_BASE = 'https://ecmc.state.co.us/weblink/results.aspx?id=XNUMBERX'
     DL_BASE = 'https://ecmc.state.co.us/weblink/XLINKX'
-    pathname = path.dirname(argv[0])
-    adir = path.abspath(pathname)
+    adir = getcwd()
     dir_add = path.join(adir,FOLDER)
 
     if FULL_UPDATE:
