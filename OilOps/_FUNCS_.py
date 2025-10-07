acd4ab3ee055aa7a006ca4809040acab313c766d
ruckerk <67653134+ruckerk@users.noreply.github.com>
2025-10-07 09:16:32 -0600

    Update _FUNCS_.py

--- OilOps/_FUNCS_.py
+++ OilOps/_FUNCS_.py
@@ -17,7 +17,6 @@ from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.support.ui import Select
-from typing import Union
 
 import subprocess
 
