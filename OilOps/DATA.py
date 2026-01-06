1e9c46e184e799c6251a5525c4afa85ef8d36dfa
ruckerk <67653134+ruckerk@users.noreply.github.com>
2026-01-06 00:10:41 -0700

    Fix connection object reference in SQL table initialization

--- OilOps/DATA.py
+++ OilOps/DATA.py
@@ -3294,7 +3294,7 @@ def ABS_LOC(DB_NAME = 'FIELD_DATA.db'):
                     'XFEET':'REAL',
                     'YFEET':'REAL'}
 
-        INIT_SQL_TABLE(connection_obj, 'SHL', LOC_COLS)
+        INIT_SQL_TABLE(CONN, 'SHL', LOC_COLS)
         LOC_DF[['UWI10','X','Y','XFEET','YFEET']].to_sql(name = 'SHL', con = CONN, if_exists='replace', index = False, dtype = LOC_COLS)
     return LOC_DF
  
